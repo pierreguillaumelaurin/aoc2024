b4553026599a117acb3a33f3be957565f3f5afd1
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-01 18:42:44 -0500

    Day 1 part 1

--- lib/2024/day01.rb
+++ lib/2024/day01.rb
@@ -1,10 +1,21 @@
 # frozen_string_literal: true
 
-module Year2023
+module Year2024
   module Day01
     class << self
-      def part1
-        puts 'works!'
+      def part1(input)
+        first = []
+        second = []
+
+        input.body.chomp.lines.each do |line|
+          f, s = line.split('   ')
+          first << f
+          second << s
+        end
+
+        first.sort!
+        second.sort!
+        first.zip(second).sum { |f, s| (s.to_i - f.to_i).abs }
       end
 
       def part2

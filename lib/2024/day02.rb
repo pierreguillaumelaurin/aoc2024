29780041f8dabfd4e5361d282a6edb213f53eff8
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-02 20:49:34 -0500

    Day 2 part 2

--- lib/2024/day02.rb
+++ lib/2024/day02.rb
@@ -10,19 +10,28 @@ module Year2024
 
       def part2(input)
         levels = parse(input)
-        levels.select do |level|
-          (level.each_cons(2).all? { |a, b| a < b } or
-        level.each_cons(2).all? { |a, b| a > b }) and
-            level.each_cons(2).all? { |a, b| (a - b).abs >= 1 && (a - b).abs <= 3 }
-        end.length
+        levels.select(&method(:tolerates_one_bad_level?)).length
       end
 
       private
 
+      def at_most_one_mistake?(level)
+        no_mistakes?(level) || one_mistake?(level)
+      end
+
       def no_mistakes?(level)
-        (level.each_cons(2).all? { |a, b| a < b } or
-      level.each_cons(2).all? { |a, b| a > b }) and
+        (level.each_cons(2).all? { |a, b| a < b } ||
+      level.each_cons(2).all? { |a, b| a > b }) &&
           level.each_cons(2).all? { |a, b| (a - b).abs >= 1 && (a - b).abs <= 3 }
+      end
+
+      def tolerates_one_bad_level?(level)
+        removals = (0...level.length).count do |index|
+          test_level = level[0...index] + level[index + 1..]
+          no_mistakes?(test_level)
+        end
+
+        removals.positive?
       end
 
       def parse(input)

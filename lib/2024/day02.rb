111d4fab504345143a0ca233487d5175ae7c5c56
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-02 20:30:18 -0500

    Refactor Day 1 part 1

--- lib/2024/day02.rb
+++ lib/2024/day02.rb
@@ -5,6 +5,11 @@ module Year2024
     class << self
       def part1(input)
         levels = parse(input)
+        levels.select(&method(:no_mistakes?)).length
+      end
+
+      def part2(input)
+        levels = parse(input)
         levels.select do |level|
           (level.each_cons(2).all? { |a, b| a < b } or
         level.each_cons(2).all? { |a, b| a > b }) and
@@ -12,28 +17,13 @@ module Year2024
         end.length
       end
 
-      def part2(input)
-        first = []
-        second = []
-
-        input.body.chomp.lines.each do |line|
-          f, s = line.split('   ')
-          first << f.to_i
-          second << s.to_i
-        end
-
-        first.sort!
-        second.sort!
-
-        answer = 0
-        first.each do |n|
-          answer += n * second.count(n)
-        end
-
-        answer
-      end
-
       private
+
+      def no_mistakes?(level)
+        (level.each_cons(2).all? { |a, b| a < b } or
+      level.each_cons(2).all? { |a, b| a > b }) and
+          level.each_cons(2).all? { |a, b| (a - b).abs >= 1 && (a - b).abs <= 3 }
+      end
 
       def parse(input)
         lines = input.body.chomp.lines

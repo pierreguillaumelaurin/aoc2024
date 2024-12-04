a8cff2fb2c0ed3faab1a10a937e9768523c2a47a
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-03 21:00:15 -0500

    Day 3 part 2

--- lib/2024/day03.rb
+++ lib/2024/day03.rb
@@ -4,16 +4,23 @@ module Year2024
   module Day03
     class << self
       def part1(input)
-        matches = input.body.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
+        run input.body
+      end
+
+      def part2(input)
+        cleaned = input.body.gsub(/don't\(\).*?do\(\)/m, 'do()')
+        run cleaned
+      end
+
+      private
+
+      def run(body)
+        matches = body.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
 
         matches.inject(0) do |sum, match|
           l, r = match
           sum + l.to_i * r.to_i
         end
-      end
-
-      def part2(input)
-        true
       end
     end
   end

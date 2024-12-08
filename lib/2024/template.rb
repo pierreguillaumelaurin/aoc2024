13f3fc9260815920d069df29c7c11b5e99e73550
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-08 13:30:57 -0500

    Add template

---
+++ lib/2024/template.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require_relative '../utils/compass'
+require_relative '../utils/grid'
+require_relative '../utils/coordinate'
+
+module Year2024
+  module Day05
+    class << self
+      def part1(input) end
+
+      def part2(input) end
+    end
+  end
+end

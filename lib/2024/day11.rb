04e3812f4a16426952349321a76bb6197fc91e62
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-24 10:39:56 -0500

    Day 11 part 2

--- lib/2024/day11.rb
+++ lib/2024/day11.rb
@@ -4,33 +4,44 @@ module Year2024
   module Day11
     class << self
       def part1(input)
-        stones = input.body.chomp.split
+        stones = input.body.chomp.split.map { |stone| [stone, 1] }.to_h
 
         25.times do
           stones = blink(stones)
         end
 
-        stones.count
+        stones.values.sum
       end
 
-      def part2(input) end
+      def part2(input)
+        stones = input.body.chomp.split.map { |stone| [stone, 1] }.to_h
+
+        75.times do
+          stones = blink(stones)
+        end
+
+        stones.values.sum
+      end
 
       private
 
       def blink(stones)
-        stones.flat_map do |stone|
+        next_stones = Hash.new(0)
+        stones.each do |stone, n|
           if stone.to_i.zero?
-            '1'
+            next_stones['1'] += 1 * n
           elsif stone.length.even?
             mid = stone.length / 2
             first = stone[0...mid]
-            second = stone[mid..]
+            second = stone[mid..].to_i.to_s
 
-            [first, second.to_i.to_s]
+            next_stones[first] += n
+            next_stones[second] += n
           else
-            stone.to_i.*(2024).to_s
+            next_stones[stone.to_i.*(2024).to_s] += n
           end
         end
+        next_stones
       end
     end
   end

efa9102d3bad03a48bab4f1f9292255a0f860378
pierreguillaumelaurin <pierreguillaumelaurin@gmail.com>
2024-12-24 17:14:35 -0500

    Day 12 part 2

--- lib/2024/day12.rb
+++ lib/2024/day12.rb
@@ -11,7 +11,10 @@ module Year2024
         garden.fencing_price
       end
 
-      def part2(input) end
+      def part2(input)
+        garden = Garden.new(Grid.from_string(input.body.chomp))
+        garden.fencing_price_with_discount
+      end
 
       class Region
         def initialize(origin, garden)
@@ -19,6 +22,7 @@ module Year2024
           @garden = garden
           @area = 0
           @perimeter = 0
+          @border_coordinates = Set.new
           @name = @garden[origin]
 
           dfs(origin)
@@ -28,7 +32,24 @@ module Year2024
           @area * @perimeter
         end
 
+        def price_with_discount
+          @area * sides
+        end
+
         private
+
+        def sides
+          result = 0
+
+          @border_coordinates.each do |pos|
+            horizontal_neighbors = @border_coordinates.count { |other| other.x == pos.x && (other.y - pos.y).abs == 1 }
+            vertical_neighbors = @border_coordinates.count { |other| other.y == pos.y && (other.x - pos.x).abs == 1 }
+
+            result += 1 if horizontal_neighbors.zero? || vertical_neighbors.zero?
+          end
+
+          result
+        end
 
         def dfs(pos)
           return if @garden.seen.include? pos
@@ -40,6 +61,8 @@ module Year2024
           @perimeter += foreign_neighbors.count
           @perimeter += 1 if pos.x.zero? || pos.x == @garden.dimensions[0] - 1
           @perimeter += 1 if pos.y.zero? || pos.y == @garden.dimensions[1] - 1
+
+          @border_coordinates.add(pos) if foreign_neighbors.count.positive?
 
           neighbors_to_visit = @garden.orthogonal_neighbors(pos)
                                       .reject { |p| @garden.seen.include?(p) }
@@ -65,7 +88,11 @@ module Year2024
         end
 
         def fencing_price
-          @regions.values.sum
+          @regions.values.sum { |price, _| price }
+        end
+
+        def fencing_price_with_discount
+          @regions.values.sum { |_, discounted_price| discounted_price }
         end
 
         private
@@ -75,7 +102,7 @@ module Year2024
             next if @seen.include? pos
 
             region = Region.new(pos, self)
-            @regions[pos] = region.price
+            @regions[pos] = [region.price, region.price_with_discount]
           end
         end
       end
